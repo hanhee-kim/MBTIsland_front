6817b51c2efaa677751e7d8cc06bbd4790a07932
hanhee-kim <914gksl914@naver.com>
2023-11-17 18:00:04 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import Main from "./component/Main";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Provider store={store}> */}
+      {/* <PersistGate persistor={persister}> */}
+      <BrowserRouter>
+        <Main />
+        <Routes>
+          <Route exact path="/" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
+      {/* </PersistGate> */}
+      {/* </Provider> */}
     </div>
   );
 }
