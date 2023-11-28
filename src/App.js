9d5f2f8c9df188efff62ae447e0cc926608511a3
hanhee-kim <914gksl914@naver.com>
2023-11-28 18:10:52 +0900

    폴더 추가 + 경로 변경

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/lib/integration/react";
 import Main from "./component/Main";
-import Join from "./component/Join";
-import Login from './component/Login';
-import AddJoin from "./component/AddJoin";
+import Login from "./component/user/Login";
+import AddJoin from "./component/user/AddJoin";
 
 import MBTmi from "./component/mbtmi/MBTmi";
 import Notice from "./component/notice/NoticeList";
 import NoticeDetail from "./component/notice/NoticeDeatil";
 import AdminNotice from "./component/admin/AdminNotice";
-import AdminNoticeForm from './component/admin/AdminNoticeForm';
-import Join from './component/user/Join';
+import AdminNoticeForm from "./component/admin/AdminNoticeForm";
+import Join from "./component/user/Join";
 import Header from "./component/common/Header";
-import Footer from './component/common/Footer';
+import Footer from "./component/common/Footer";
 
 function App() {
   return (
@@ -23,20 +22,20 @@ function App() {
       {/* <Provider store={store}> */}
       {/* <PersistGate persistor={persister}> */}
       <BrowserRouter>
-        <Main/>
-        <Header/>
+        <Main />
+        <Header />
         <Routes>
-          <Route exact path="/notice" element={<Notice/>} />
-          <Route exact path="/noticeDetail" element={<NoticeDetail/>} />
-          <Route exact path="/adminNotice" element={<AdminNotice/>} />
-          <Route exact path="/adminNoticeForm" element={<AdminNoticeForm/>} />
-          <Route exact path="/mbtmi" element={<MBTmi/>} />
+          <Route exact path="/notice" element={<Notice />} />
+          <Route exact path="/noticeDetail" element={<NoticeDetail />} />
+          <Route exact path="/adminNotice" element={<AdminNotice />} />
+          <Route exact path="/adminNoticeForm" element={<AdminNoticeForm />} />
+          <Route exact path="/mbtmi" element={<MBTmi />} />
           <Route exact path="/" element={<Main />} />
           <Route exact path="/join" element={<Join />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/addjoin" element={<AddJoin />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
       {/* </PersistGate> */}
       {/* </Provider> */}
