c395210fef371573c8efc699df96ea0740b8acd1
yhayeong <staccatissimo123@gmail.com>
2023-11-28 17:47:15 +0900

    public 이미지파일 추가

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/lib/integration/react";
 import Main from "./component/Main";
-import Join from "./component/Join";
+
+import MBTmi from "./component/mbtmi/MBTmi";
+import Notice from "./component/notice/NoticeList";
+import NoticeDetail from "./component/notice/NoticeDeatil";
+import AdminNotice from "./component/admin/AdminNotice";
+import AdminNoticeForm from './component/admin/AdminNoticeForm';
+import Join from './component/user/Join';
+import Header from "./component/common/Header";
+import Footer from './component/common/Footer';
 
 function App() {
   return (
@@ -12,10 +20,17 @@ function App() {
       {/* <Provider store={store}> */}
       {/* <PersistGate persistor={persister}> */}
       <BrowserRouter>
-        <Main />
+        <Main/>
+        <Header/>
         <Routes>
-          <Route exact path="/" element={<Join />} />
+          <Route exact path="/" element={<Join/>} />
+          <Route exact path="/notice" element={<Notice/>} />
+          <Route exact path="/noticeDetail" element={<NoticeDetail/>} />
+          <Route exact path="/adminNotice" element={<AdminNotice/>} />
+          <Route exact path="/adminNoticeForm" element={<AdminNoticeForm/>} />
+          <Route exact path="/mbtmi" element={<MBTmi/>} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
       {/* </PersistGate> */}
       {/* </Provider> */}
