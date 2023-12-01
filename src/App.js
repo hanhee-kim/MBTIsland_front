a484ff154477681f318af58f45ebd740be9f2436
hanhee-kim <914gksl914@naver.com>
2023-12-01 10:51:53 +0900

    무늬 포도 !

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, useLocation } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/lib/integration/react";
 import Main from "./component/Main";
@@ -26,21 +26,28 @@ import DefaultMypage from "./component/user/DefaultMypage";
 import MbtyWrite from "./component/mbty/MbtyWrite";
 import MbtyModify from "./component/mbty/MbtyModify";
 import QnAWrite from "./component/user/QnAWrite";
+import { useState } from "react";
 
 function App() {
+  const [isPopup, setIsPopup] = useState(false);
+
   return (
     <div className="App">
       {/* <Provider store={store}> */}
       {/* <PersistGate persistor={persister}> */}
       <BrowserRouter>
-        <Header />
+        {!isPopup && <Header />}
         <Routes>
           {/* 한희 */}
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/join" element={<Join />} />
           <Route exact path="/addjoin" element={<AddJoin />} />
           <Route exact path="/mypage" element={<Mypage />} />
-          <Route exact path="/qnaWrite" element={<QnAWrite />} />
+          <Route
+            exact
+            path="/qnaWrite"
+            element={<QnAWrite setIsPopup={setIsPopup} />}
+          />
           {/* 하영 */}
           <Route exact path="/" element={<Main />} />
           <Route exact path="/mbtmi" element={<MBTmi />} />
@@ -59,7 +66,7 @@ function App() {
           <Route exact path="/mbtywrite" element={<MbtyWrite />} />
           <Route exact path="/mbtymodify" element={<MbtyModify />} />
         </Routes>
-        <Footer />
+        {!isPopup && <Footer />}
       </BrowserRouter>
       {/* </PersistGate> */}
       {/* </Provider> */}
