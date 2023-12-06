2f96275fedafd786547dca2fde77bd386b8b8001
hanhee-kim <914gksl914@naver.com>
2023-12-07 08:21:24 +0900

    proxy추가 + join.js 수정

--- src/component/user/Join.js
+++ src/component/user/Join.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { useNavigate } from "react-router";
 import { Button, Col, Form, FormGroup, Input, Label } from "reactstrap";
 import Swal from "sweetalert2";
 
@@ -54,6 +55,10 @@ const Join = () => {
   });
   //비밀번호확인값
   const [passwordCheckValue,setPasswordCheckValue] = useState('');
+  //serverEmailCode
+  const [serverEmailCode,setServerEmailCode] = useState('');
+  //사용자가 작성한 emailCode
+  const [emailCode,setEmailcode] = useState('');
   
   const [mbtiCheckEI, setMbtiCheckEI] = useState("E");
   const [mbtiCheckNS, setMbtiCheckNS] = useState("N");
@@ -77,8 +82,22 @@ const Join = () => {
     var nickRegExp = /^[a-zA-Z0-9가-힣]{2,8}$/;
     return nickRegExp.test(user.userNickname);
   };
+  //Email의 유효성조건
+  const validateUserEmail = () => {
+    var emailRegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegExp.test(user.userEmail);
+  }
+
+  //emailCode값 입력할떄
+  const changeEmailcode = (e) => {
+    setEmailcode(e.target.value);
+    if(isEmailCheck){
+      setIsEmailCheck(false);
+    }
+  }
   //Input값이 변경될때
   const change = (e) => {
+    console.log("input:" + e.target.value);
     setUser({
       ...user,
       [e.target.name]: [e.target.value],
@@ -93,9 +112,13 @@ const Join = () => {
         setIsSamePassword(true);
       }
     }
-    console.log("input:" + e.target.value);
-    // console.log("user" + user);
-    // console.log("username:" + user.username + "  " + user.userPassword);
+    if(e.target.name === 'username'){
+      setIsIdDuplicateCheck(false);
+    }
+    if(e.target.name === 'userEmail'){
+      setIsEmailCheck(false);
+    }
+    
   };
   //비밀번호 확인란 입력할 때
   const passwordCheck = (e) => {
@@ -113,28 +136,67 @@ const Join = () => {
     e.preventDefault(); //기본 제출 막기
     const isIdValid = validateUsername(); //id 유효성 통과하는지
     const isNickValid = validateUserNick(); //닉네임 유효성 통과하는지
-    //id중복체크했는지
-    if(!isIdDuplicateCheck){
+  if(isIdValid){  //id유효성
+    if(isNickValid){  //닉네임 유효성
+      if(isIdDuplicateCheck){ //id 중복체크
+        if(!user.userPassword.length<4){ // 비밀번호 길이 4자 이하인지
+          if(isSamePassword){
+            if(isEmailCheck){
+              let sendUser = {
+                ...user,
+                muserMbti: mbtiCheckEI + mbtiCheckNS + mbtiCheckTF + mbtiCheckPJ,
+              };
+              axios
+                .post('http://localhost:8090/join',sendUser)
+                .then((res)=>{
+                  console.log(res);
+                  window.location.href('/login');
+                })
+                .catch((err)=>{
+                  console.log(err);
+                })
+              
+            }else{
+              Swal.fire({
+                title:'이메일 인증을 해주세요.',
+                icon:'warning',
+              })
+            }
+          }else{
+            Swal.fire({
+              title:'비밀번호가 일치하지 않습니다.',
+              icon:'warning',
+            })
+          }
+        }else{
+          Swal.fire({
+            title:'비밀번호를 확인해주세요.',
+            text:'비밀번호는 4자이상작성해주세요.',
+            icon:'warning',
+          })
+        }
+      }else{
+        Swal.fire({
+          title:'ID가 중복됩니다.',
+          icon:'warning',
+        })
+      }
 
-    }
-    //email 인증했는지
-    if(!isEmailCheck){
-
-    }
-    //비밀번호 길이 4자 이하인지
-    if(user.userPassword.length<4){
+    }else{
       Swal.fire({
-
+        title:'닉네임을 확인해주세요.',
+        text:'닉네임은 특수문자 제외 이루어진 2~8자입니다.',
+        icon:'warning',
       })
     }
-    // console.log(isIdValid);
+  }else{
+    Swal.fire({
+      title:'ID를 확인해주세요.',
+      text:'ID는 영문 숫자로 이루어진 4~8자입니다.',
+      icon:'warning',
+    })
+  }
     
-
-
-    let sendUser = {
-      ...user,
-      muserMbti: mbtiCheckEI + mbtiCheckNS + mbtiCheckTF + mbtiCheckPJ,
-    };
 
 
   };
@@ -145,20 +207,61 @@ const Join = () => {
             .get(`http://localhost:8090/duplicate/${user.username}`)
             .then((res) => {
                 console.log(res);
-                // Swal.fire(res.data);
+                if(res.data == '사용가능'){
+                  Swal.fire({
+                    title:'사용가능한 ID입니다.',
+                    icon:"success",
+                  });
+                  setIsIdDuplicateCheck(true);
+                }else if(res.data == '사용불가'){
+                  Swal.fire({
+                    title:'ID가 중복됩니다.',
+                    icon:"warning",
+                  });
+                }
             })
             .catch((err) => {
                 console.log(err);
-                // Swal.fire(err.response.data);
+                Swal.fire({
+                  title:'ID가 중복됩니다.',
+                  icon:"warning",
+                });
             });
   }
   //email작성후 보내기버튼 눌렀을때(이때 이메일 중복여부도 확인해주어야함.)
   const sendCode = (e) =>{
     e.preventDefault();
+    console.log("valiEmail"+validateUserEmail());
+    if(!validateUserEmail()){
+      Swal.fire({
+        title:'Email을 올바르게 작성해주세요.',
+        icon:'warning',
+      })
+      return;
+    }
+    axios
+      .get(`http://localhost:8090/sendmail/${user.userEmail}`)
+      .then((res) => {
+          console.log(res);
+          setServerEmailCode(res.data);
+          console.log("serverCode : "+res.data);
+          Swal.fire({
+            title:'인증코드가 전송되었습니다.',
+            text:'작성하신 Email을 확인해주세요.',
+            icon:'success',
+          })
+      })
+      .catch((err) => {
+          console.log(err);
+      });
   }
   //email 코드입력후 인증버튼 눌렀을때
-  const emailConform = (e) => {
+  const emailConfirm = (e) => {
     e.preventDefault();
+    if(serverEmailCode === emailCode){
+      console.log('code일치!');
+      setIsEmailCheck(true);
+    }
   }
   //E I 라벨 눌렀을때
   const mbtiEIClick = (e, ei) => {
@@ -205,6 +308,7 @@ const Join = () => {
               id="username"
               maxLength={8}
               placeholder="ID를 입력하세요."
+              required
               onChange={change}
             />
           </Col>
@@ -230,6 +334,7 @@ const Join = () => {
               placeholder="PASSWORD를 입력하세요."
               minLength={4}
               maxLength={12}
+              required
               onChange={change}
             />
           </Col>
@@ -246,6 +351,7 @@ const Join = () => {
               placeholder="PASSWORD를 한번더 입력하세요."
               minLength={4}
               maxLength={12}
+              required
               onChange={(e)=>passwordCheck(e)}
             />
           </Col>
@@ -262,6 +368,7 @@ const Join = () => {
               id="userNickname"
               placeholder="NICKNAME 입력하세요."
               maxLength={8}
+              required
               onChange={change}
             />
           </Col>
@@ -362,10 +469,11 @@ const Join = () => {
           </Label>
           <Col sm={7}>
             <Input
-              type="text"
+              type="email"
               name="userEmail"
               id="userEmail"
               placeholder="Email 입력하세요."
+              required
               onChange={change}
             />
           </Col>
@@ -389,14 +497,16 @@ const Join = () => {
               name="join_code"
               id="join_code"
               placeholder="인증코드를 입력하세요."
-              onChange={change}
+              required
+              maxLength={8}
+              onChange={(e)=>changeEmailcode(e)}
             />
           </Col>
           <Col sm={2}>
             <Button
               color="white"
               style={{ border: "1px solid black", fontWeight: "600" }}
-              onClick={(e)=>emailConform(e)}
+              onClick={(e)=>emailConfirm(e)}
             >
               인증
             </Button>
