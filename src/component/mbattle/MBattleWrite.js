78e95e9222a1d56074984b85bcda127d6a158d0a
nameisris <insoo5110@gnail.com>
2023-12-21 11:44:27 +0900

    mbattlewrite, 파일 업로드

--- src/component/mbattle/MBattleWrite.js
+++ src/component/mbattle/MBattleWrite.js
@@ -22,8 +22,8 @@ function MBattleWrite() {
         title:"",
         voteItem1:"",
         voteItem2:"",
-        file1idx:"",
-        file2idx:"",
+        fileIdx1:"",
+        fileIdx2:"",
         writerId: user.username,
         writerNickname: user.userNickname,
         writerMbti: user.userMbti,
@@ -44,24 +44,35 @@ function MBattleWrite() {
     const fileChange = (e) => {
         const name = e.target.name;
         const value = e.target.value
+
         // 실제 추가된 파일이 있을 때 (파일의 크기가 0보다 클 때)
         // 파일 디렉토리를 열었지만 취소 버튼을 클릭한 경우
         if(e.target.files.length > 0) {
+            const newFiles = [...files];
             // files 배열에 추가
             // 얕은 복사로 추가할 파일을 배열에 넣음
+            if(name==="fileIdx1") {
+                newFiles[0] = e.target.files[0];
+                setFiles(newFiles);
+                // setFiles([...files, e.target.files[0]]);
+            } else if (name==="fileIdx2") {
+                newFiles[1] = e.target.files[0];
+                setFiles(newFiles);
+                // setFiles([...files, e.target.files[0]]);
+            }
             setMbattle({...mbattle, [name]:value});
-            setFiles([...files, e.target.files[0]]);
+            console.log(files);
         }
     
         // 기존 img 태그 영역의 이미지를 변경하는 코드
         // 파라미터 e에서 file 경로를 가져옴
         if(e.target.files!==null) {
             const imageSrc = URL.createObjectURL(e.target.files[0]); // img 태그에서 가리키는 imgBoxRef에 file 경로 설정
-            if(name==="file1idx") {
+            if(name==="fileIdx1") {
                 imgBoxRef1.current.src = imageSrc;
                 imgBoxRef1.current.width = 290;
                 imgBoxRef1.current.height = 290;
-            } else if(name==="file2idx") {
+            } else if(name==="fileIdx2") {
                 imgBoxRef2.current.src = imageSrc;
                 imgBoxRef2.current.width = 300;
                 imgBoxRef2.current.height = 300;
@@ -72,8 +83,24 @@ function MBattleWrite() {
     // 게시글 작성
     const postMbattle = () => {
         // formData.append("file", files);
+        const formData = new FormData();
+        formData.append("title", mbattle.title);
+        formData.append("voteItem1", mbattle.voteItem1);
+        formData.append("voteItem2", mbattle.voteItem2);
+        formData.append("fileIdx1", mbattle.fileIdx1);
+        formData.append("fileIdx2", mbattle.fileIdx2);
+        formData.append("writerId", mbattle.writerId);
+        formData.append("writerNickname", mbattle.writerNickname);
+        formData.append("writerMbti", mbattle.writerMbti);
+        formData.append("writerMbtiColor", mbattle.writerMbtiColor);
+        
+        for(let file of files) {
+            formData.append("files", file);
+        }
+
+        console.log("폼데이터", formData);
     
-        axios.post("http://localhost:8090/mbattlewrite/", mbattle)
+        axios.post("http://localhost:8090/mbattlewrite/", formData)
         .then(res=> {
             console.log(res);
             let no = res.data.no;
@@ -136,9 +163,9 @@ function MBattleWrite() {
                     {/* 사진, 주제 작성 */}
                     <div className={style.sectionSubject}>
                         <div>
-                            <div className={style.inputFileDiv} onClick={()=>document.getElementById("file1Idx").click()} >
+                            <div className={style.inputFileDiv} onClick={()=>document.getElementById("fileIdx1").click()} >
                                 <img src="/attachIcon.png" alt="" ref={imgBoxRef1} width="200px" height="200px"/>
-                                    <Input type="file" id="file1idx" name="file1idx" accept="image/*" onChange={fileChange} hidden/>
+                                    <Input type="file" id="fileIdx1" name="fileIdx1" accept="image/*" onChange={fileChange} hidden/>
                             </div>
                             항목1
                             <Input 
@@ -155,9 +182,9 @@ function MBattleWrite() {
                             />
                         </div>
                         <div>
-                            <div className={style.inputFileDiv} onClick={()=>document.getElementById("file2Idx").click()}>
+                            <div className={style.inputFileDiv} onClick={()=>document.getElementById("fileIdx2").click()}>
                                 <img src="/attachIcon.png" alt="" ref={imgBoxRef2} width="200px" height="200px"/>
-                                    <Input type="file" id="file2idx" name="file2idx" accept="image/*" onChange={fileChange} hidden/>
+                                    <Input type="file" id="fileIdx2" name="fileIdx2" accept="image/*" onChange={fileChange} hidden/>
                             </div>
                             항목2
                             <Input 
