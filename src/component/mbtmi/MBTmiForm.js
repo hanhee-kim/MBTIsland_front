e18722973c95725087769b6f4eaf916857dc06e2
yhayeong <staccatissimo123@gmail.com>
2023-12-21 19:06:34 +0900

    효효효...

--- src/component/mbtmi/MBTmiForm.js
+++ src/component/mbtmi/MBTmiForm.js
@@ -27,6 +27,13 @@ const MBTmiForm = () => {
     const [activeCategory, setActiveCategory] = useState("");
 
 
+
+
+    // 선택된 이미지 (백엔드로 보내어 처리 전)
+    const [selectedImage, setSelectedImage] = useState(null);
+
+
+
     // 에디터ref등록
     const quillRef = useRef();
 
@@ -38,14 +45,13 @@ const MBTmiForm = () => {
 
 
 
+
+
     // 에디터 content의 변경이벤트 감지하여 호출됨... 이미지 삽입 감지하도록함=삽입이미지핸들러함수가 이미지를 처리하도록함(서버에 저장하고 url을 받아와서...)
     const handleQuillChange = async (content, delta, source, editor) => {
-        // setQuillValue(editor.getContents());
-        // setQuillValue(editor.getHTML());
         setQuillValue(content);
 
         console.log('editor.getHTML():', editor.getHTML()); // 텍스트는 <p>입력문자열</p> 이미지는 <img src="엄청 긴 base64코드"> 로 콘솔에 찍힌다
-        // console.log('editor.getContents():', editor.getContents());
 
 
         // 이 함수를 트리거하는 이벤트인 onChange가 이미지 삽입으로 인해서 일어났는지를 체크
@@ -60,25 +66,59 @@ const MBTmiForm = () => {
     
                 input.addEventListener("change", async () => {
                     const file = input.files[0];
-    
-                    try {
-                        const formData = new FormData();
-                        formData.append("image", file);
-    
-                        const response = await axios.post('http://localhost:8090/uploadImage', formData);
-    
-                        const editor = quillRef.current.getEditor();
-                        editor.clipboard.dangerouslyPasteHTML(response.data.imageUrl, "api");
-    
-                    } catch (error) {
-                        console.error("에러:", error);
-                    }
+                    setSelectedImage(file);
+                    
                 });
     
                 input.click();
             }
         }
     };
+
+
+    
+
+    // 외부버튼클릭
+    const btnOutsideClick = async () => {
+        console.log('외부버튼 클릭');
+
+        // 선택된 이미지가 있는 경우에만 처리
+        if (selectedImage) {
+            try {
+            const formData = new FormData();
+            formData.append('image', selectedImage); // state에 저장해둔 처리 전의 선택된 이미지를 폼데이터로 만듦
+    
+            const response = await axios.post('http://localhost:8090/uploadImage', formData);
+    
+            const editor = quillRef.current.getEditor();
+            editor.clipboard.dangerouslyPasteHTML(response.data.imageUrl, 'api');
+    
+            // 이미지 처리 후 state 초기화
+            setSelectedImage(null);
+            } catch (error) {
+            console.error('에러:', error);
+            }
+        }
+        
+        // 서버로 보내기
+        try {
+            const response = await axios.post(`http://localhost:8090/quilltest`, {
+                content: quillValue,
+                title: '퀼테스트제목',
+                category: '잡담',
+                writerId: user.username,
+                writerNickname: user.userNickname,
+                writerMbti: user.userMbti,
+                writerMbtiColor: user.userMbtiColor,
+                
+                
+            });
+            console.log('퀼데이터보내기 요청결과: ', response);
+            
+        } catch (error) {
+            console.error('퀼데이터보내기 오류내용: ', error);
+        }
+    }
 
 
     // 리액트quill의 기능을 확장 또는 조정
@@ -121,30 +161,6 @@ const MBTmiForm = () => {
 
 
 
-
-    // 외부버튼클릭
-    const btnOutsideClick = async () => {
-        console.log('외부버튼 클릭');
-        
-        // 서버로 보내기
-        try {
-            const response = await axios.post(`http://localhost:8090/quilltest`, {
-                content: quillValue,
-                title: '퀼테스트제목',
-                category: '잡담',
-                writerId: user.username,
-                writerNickname: user.userNickname,
-                writerMbti: user.userMbti,
-                writerMbtiColor: user.userMbtiColor,
-                
-                
-            });
-            console.log('퀼데이터보내기 요청결과: ', response);
-            
-        } catch (error) {
-            console.error('퀼데이터보내기 오류내용: ', error);
-        }
-    }
 
 
 
