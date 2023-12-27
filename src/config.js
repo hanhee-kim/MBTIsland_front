c7bc933bd76b0f639ebc74801c8b37c7eef3f46b
yhayeong <staccatissimo123@gmail.com>
2023-12-27 16:50:24 +0900

    1227 4pm 배포버전

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-// export const urlroot = "http://3.36.65.170:8090"
+export const urlroot = "http://3.38.149.112:8090"
 
-export const urlroot = "http://localhost:8090"
+// export const urlroot = "http://localhost:8090"
