9f03fd6b8f5a4bc652d4795e4b92d552f7fb33f8
yhayeong <staccatissimo123@gmail.com>
2023-12-29 01:14:21 +0900

    수정

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-// export const urlroot = "http://3.38.149.112:8090"
+export const urlroot = "http://3.38.149.112:8090"
 
-export const urlroot = "http://localhost:8090"
+// export const urlroot = "http://localhost:8090"
